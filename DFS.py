123dd9cfbcb949329a7f5efc2e37707c6c3d617e
Jhansiveni <jhansivenidesu>
2026-06-28 19:07:36 +0530

    wrote the main code

--- DFS.py
+++ DFS.py
@@ -1,2 +1,12 @@
 # DFS Traversal code
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.children=[]
+def dfs(root):
+    if not root:
+        return
+    print(root.value,end=" ")
 
+    for child in root.children:
+        dfs(child)
